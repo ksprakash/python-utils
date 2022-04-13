091e87c87e1eee6ecb76edc4f35a1a94ce8353f1
SaiPrakash <SaiPrakash.Kokkiligadda@coxautoinc.com>
2022-04-13 15:04:45 -0400

    Added class Utility

--- utility.py
+++ utility.py
@@ -1,18 +1,20 @@
 import os
 import json
 
-def get_location():
-    location = os.path.dirname(os.path.realpath(__file__)
-    return location
+class Utility(object):
 
-def get_data(txt):
-    data = json.load(txt)
-    return data
+    def get_location(self):
+       location = os.path.dirname(os.path.realpath(__file__)
+       return location
 
-def get_user_in_lower(name):
-    name=name.lower()
-    return name
+    def get_data(self,txt):
+       data = json.load(txt)
+       return data
 
-def get_user_in_capital(name):
-    name=name.upper()
-    return name
+    def get_user_in_lower(self,name):
+       name=name.lower()
+       return name
+
+    def get_user_in_capital(self,name):
+       name=name.upper()
+       return name
