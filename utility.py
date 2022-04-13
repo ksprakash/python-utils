69224e23dfa6a22c36e968527992149b162cbad1
SaiPrakash <SaiPrakash.Kokkiligadda@coxautoinc.com>
2022-04-13 12:11:10 -0400

    function for getting dictionary added

--- utility.py
+++ utility.py
@@ -4,3 +4,7 @@ import json
 def get_location():
     location = os.path.dirname(os.path.realpath(__file__)
     return location
+
+def get_data(str_txt):
+    data = json.load(str_txt)
+    return data
